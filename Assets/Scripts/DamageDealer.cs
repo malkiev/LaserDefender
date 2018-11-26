ad3ad4486483a33c81dfed11f522dff3019bb29a
Kevin Attard <kevin.attard@gmail.com>
2018-11-26 23:38:30 +0100

    First commit

---
+++ Assets/Scripts/DamageDealer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDealer : MonoBehaviour {
+
+    [SerializeField] int damage = 100;
+
+    public int GetDamage()
+    {
+        return damage;
+    }
+
+    public void Hit()
+    {
+        Destroy(gameObject);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] float health = 100;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] GameObject enemyProjectile;
+    [SerializeField] float projectileSpeed = 10;
+    [SerializeField] float durationOfExplosion = 1f;
+    [SerializeField] GameObject explosionVfx;
+
+    // Use this for initialization
+    void Start () {
+        shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CountDownAndShoot();
+	}
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+
+        if (shotCounter <= 0)
+        {
+            Fire();
+
+            shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject laser = Instantiate(enemyProjectile, gameObject.transform.position, Quaternion.identity);
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+
+        if (damageDealer != null)
+        {
+            
+            ProcessHit(damageDealer);
+        }
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+        damageDealer.Hit();
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+        GameObject explsion = Instantiate(explosionVfx, transform.position, Quaternion.identity);
+        Destroy(explsion, durationOfExplosion);
+    }
+}
